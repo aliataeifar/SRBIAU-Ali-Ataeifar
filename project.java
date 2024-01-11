bb68d6d28397856f7df9f0957b23951c32aefc88
aliataeifar <59919358+aliataeifar@users.noreply.github.com>
2024-01-11 06:43:29 +0330

    Update project.java

---
+++ project.java
@@ -0,0 +1,23 @@
+class Employee{
+    float salary=52000000;
+}
+class Programmer extends Employee{
+    int bonus=6000000;
+    public static void main(String args[]){
+        Programmer p=new Programmer();
+        System.out.println("Programmer salary is:"+p.salary);
+        System.out.println("Bonus of Programmer is:"+p.bonus);
+    }
+}
+public class Main {
+  String fname = "ALi";
+  String lname = "Ataeifar";
+  int age = 24;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+    System.out.println("Age: " + myObj.age);
+  }
+}
+
